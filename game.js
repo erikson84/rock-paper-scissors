b2154d178a137e263261105046763d3995641959
Erikson Kaszubowski <erikson84@yahoo.com.br>
2022-08-06 16:03:59 -0300

    Implement playerChoice function with correct case handling

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3) + 1;
     let choiceString;
+
     switch (choice) {
         case 1:
             choiceString = 'rock';
@@ -16,8 +17,24 @@ function computerPlay() {
     return choiceString;
 }
 
-function playerChoice(choice) {
-
+function playerChoice() {
+    let choice = prompt("Rock, paper or scissors?\n(Or press 'Cancel' to stop)");
+    if (choice == null) {
+        alert("Thank you for playing!");
+        return choice;
+    } else {
+        choice = choice.toLowerCase();
+    }
+    switch (choice) {
+        case 'rock':
+        case 'paper':
+        case 'scissors':
+            return choice;
+        
+        default:
+            alert("Please choose a valid option");
+            return '';
+    }
 }
 
 function turn(computerChoice, playerChoice) {
