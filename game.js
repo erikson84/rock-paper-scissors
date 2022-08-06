002110f471f4a583a598154b88220a9305c46303
Erikson Kaszubowski <erikson84@yahoo.com.br>
2022-08-06 15:43:02 -0300

    Implemented computer choice function

--- game.js
+++ game.js
@@ -1,5 +1,19 @@
 function computerPlay() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    let choiceString;
+    switch (choice) {
+        case 1:
+            choiceString = 'rock';
+            break;
+        case 2:
+            choiceString = 'paper';
+            break;
+        case 3:
+            choiceString = 'scissors';
+            break;
+    }
 
+    return choiceString;
 }
 
 function playerChoice(choice) {
