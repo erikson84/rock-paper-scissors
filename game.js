d398115c5b9b8c2d12d120fcc55b05f934b29632
Erikson Kaszubowski <erikson.k@ufsc.br>
2022-08-12 14:45:38 -0300

    Refactors playerchoice function to play a single turn and update scoreboard

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ buttons.forEach(btn => btn.addEventListener("mouseenter", highlightButton));
 buttons.forEach(btn => btn.addEventListener("mouseleave", unlightButton));
 buttons.forEach(btn => btn.addEventListener("mousedown", clickButton));
 buttons.forEach(btn => btn.addEventListener("mouseup", unclickButton));
+buttons.forEach(btn => btn.addEventListener("click", playerChoice));
 
-const message = document.querySelector(".message")
 
+const message = document.querySelector(".message");
 
 function highlightButton(e) {
     this.classList.add("highlight");
@@ -45,92 +46,65 @@ function computerChoice() {
     return choiceString;
 }
 
-function playerChoice() {
-    let choice = prompt("Rock, paper or scissors?\n(Or press 'Cancel' to stop)");
-    if (choice == null) {
-        alert("Thank you for playing!");
-        return choice;
-    } else {
-        choice = choice.toLowerCase();
+function playerChoice(e) {
+    let playerHand = e.srcElement.id;
+    let computerHand = computerChoice();
+
+    const playerScoreBoard = document.querySelector("#playpoints");
+    const computerScoreBoard = document.querySelector("#comppoints");
+
+    playerScore = +playerScoreBoard.textContent;
+    computerScore = +playerScoreBoard.textContent;
+
+    let result = turn(computerHand, playerHand);
+
+    if (result == 'player') {
+        playerScore++;
+        playerScoreBoard.textContent = playerScore;
+    } else if (result == 'computer') {
+        computerScore++;
+        computerScoreBoard.textContent = computerScore;
     }
-    switch (choice) {
-        case 'rock':
-        case 'paper':
-        case 'scissors':
-            return choice;
-        
-        default:
-            alert("Please choose a valid option");
-            return playerChoice();
-    }
+
 }
 
 function turn(computerChoice, playerChoice) {
     switch (playerChoice) {
         case 'rock':
             if (computerChoice == 'paper') {
-                alert('Paper beats rock! Computer won!');
+                message.textContent = 'Paper beats rock! Computer won!';
                 return 'computer';
             } else if (computerChoice == 'scissors') {
-                alert('Rock breaks scissors! You won!');
+                message.textContent = 'Rock breaks scissors! You won!';
                 return 'player';
             } else {
-                alert("It's a tie!");
+                message.textContent = "Rock and rock! It's a tie!";
                 return 'tie';
             }
         case 'paper':
             if (computerChoice == 'scissors') {
-                alert('Scissors beats paper! Computer won!');
+                message.textContent = 'Scissors beats paper! Computer won!';
                 return 'computer';
             } else if (computerChoice == 'rock') {
-                alert('Paper wraps rock! You won!');
+                message.textContent = 'Paper wraps rock! You won!';
                 return 'player';
             } else {
-                alert("It's a tie!");
+                message.textContent = "Paper and paper! It's a tie!";
                 return 'tie';
             }
         case 'scissors':
             if (computerChoice == 'rock') {
-                alert('Rock breaks scissors! Computer won!');
+                message.textContent = 'Rock breaks scissors! Computer won!';
                 return 'computer';
             } else if (computerChoice == 'paper') {
-                alert('Scissors cut paper! You won!');
+                message.textContent = 'Scissors cut paper! You won!';
                 return 'player';
             } else {
-                alert("It's a tie!");
+                message.textContent = "Scissors and scissors! It's a tie!";
                 return 'tie';
             }
         default:
             alert('Something went terrible wrong!');
             break;
     }
-}
-
-function playGame(n=5) {
-    let computerScore = 0;
-    let playerScore = 0;
-
-    for (let g=0; g < n; g++) {
-        let playerHand = playerChoice();
-
-        if (playerHand == null) {
-            alert(`Game cancelled.\nFinal score\nComputer: ${computerScore} vs ${playerScore}: Player`);
-            return;
-        }
-
-        let computerHand = computerChoice();
-
-        result = turn(computerHand, playerHand);
-
-        if (result == 'player') {
-            playerScore++;
-        } else if (result == 'computer') {
-            computerScore++;
-        }
-
-        if (playerScore > Math.floor(n/2) || computerScore > Math.floor(n/2)) break;
-    }
-
-    alert(`Game over! ${(computerScore > playerScore)? 'The computer ':'You '} won!\nFinal Score\nComputer: ${computerScore} vs ${playerScore}: Player`);
-
 }
